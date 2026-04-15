045d7079c3160f7c5a7748d690b050411a8ed257
Joseph Musyoka <musyokajose06@gmail.com>
2026-04-15 11:03:25 +0300

    adding my first commit

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+require('datejs');
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: [],
+    merge_date: new Date().toString('M/d/yyyy')
+  };
+
+  for (const userArray of args) {
+    combinedObject.users = [...combinedObject.users, ...userArray];
+  }
+
+  return combinedObject;
+}
 
 
 module.exports = {
